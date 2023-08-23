c5b9f92bc8e53f19f58ff7fe15e42f632aa758a3
Maridon Romney <mkromney@gmail.com>
2023-08-23 17:04:09 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const cwd = process.cwd();
+
+const PORT = process.env.port || 3001;
+const app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
