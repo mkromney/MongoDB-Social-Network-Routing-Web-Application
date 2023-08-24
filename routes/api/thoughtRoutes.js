7ab53927b89acf78dca04695c28a9d6473fc4a96
Maridon Romney <mkromney@gmail.com>
2023-08-23 20:11:52 -0600

    added route files, setting up and troubleshooting.

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,11 @@
+//  /api/thoughts
+
+// GET to all thoughts
+
+// GET to a single thought by its _id.
+
+// POST to create a new thought (don't forget to push the create thought's _id to the associated user's thoughts array field)
+
+// PUT to update a thought by its _id
+
+// DELETE to pull and remove a reaction by the reaction's reactionID value
---
+++ routes/friendRoutes.js
@@ -0,0 +1,7 @@
+//  /api/users/:userId/friends/:friendId
+
+// POST to add anew friend to a user's friend list
+
+// DELETE to remove a friend from a user's friend list
+
+
